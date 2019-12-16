08e5d5df30ee14c1c6cefe8329ea230caa12bbcc
90rimane <55492850+90rimane@users.noreply.github.com>
2019-12-16 10:53:37 +0100

    Add files via upload

---
+++ start.js
@@ -0,0 +1,8 @@
+function processForm(){
+    var parameters = location.search.substring(1).split("&");
+    var temp = parameters[0].split("=");
+    l = unescape(temp[1]);
+    document.getElementById("data").innerHTML = l;
+}
+newFunction();
+
