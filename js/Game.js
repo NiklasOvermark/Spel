06ea362d7d944fe1356b4ccfd1713b0d60ee4c8c
timoyi <dreem-cos@hotmail.com>
2020-01-08 11:12:12 +0100

    file addres change

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,34 @@
 /*Game*/
+var WIDTH = 100
+var HEIGHT = 150;
+
+var time;
+var tiles = [];
+
+function setup() {
+    createCanvas(401, 601);
+
+    time = 0;
+    tiles.push(0, 1, 1, 1);
+    tiles.push(0, 1, 1, 1);
+    tiles.push(0, 1, 1, 1);
+    tiles.push(0, 1, 1, 1);
+}
+
+function draw() {
+    background(51);
+
+
+    for (var i = 0; i < tiles.length; i++) {
+        var x = (i % 4) * WIDTH;
+        var y = Math.floor(i / 4) * HEIGHT;
+
+        fill((tiles[i] === 0) ? 0 : 255)
+        rect(x, y, WIDTH, HEIGHT);
+    }
+}
+
+function newRow() {
+
+
+}
