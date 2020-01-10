44f865e8662647b022b10073e8eac2343d2f3060
Rima Aneta <90rimane@mk.se>
2020-01-10 09:07:26 +0100

    new pages created

--- js/start.js
+++ js/start.js
@@ -1,9 +1,21 @@
-function processForm(){
-    var parameters = location.search.substring(1).split("&");
-    var temp = parameters[0].split("=");
-    l = unescape(temp[1]);
-    document.getElementById("data").innerHTML = l;
+
+//function for text file(That not working in google chrome)
+function aboutPianoTiles(){
+    var xmlhttp;
+    if(window.XMLHttpRequest){
+
+        xmlhttp=new XMLHttpRequest();
+    }else{
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange=function(){
+        if(xmlhttp.readyState==4){
+            document.getElementById('result').innerHTML=xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET","about.txt",true);
+    xmlhttp.send();
 }
-newFunction();
 
 
